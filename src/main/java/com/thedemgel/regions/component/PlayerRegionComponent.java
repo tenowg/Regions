b2bd6801d2dfd578114811f07b7f43db322e821b
Craig Russell <tenowg@gmail.com>
2013-02-08 00:34:37 -0500

    Some clean up, reorganizing, and finalizing -- almost done with first release

--- src/main/java/com/thedemgel/regions/component/PlayerRegionComponent.java
+++ src/main/java/com/thedemgel/regions/component/PlayerRegionComponent.java
@@ -1,10 +1,16 @@
 package com.thedemgel.regions.component;
 
 import com.thedemgel.regions.data.Region;
+import java.util.Set;
 import org.spout.api.component.type.EntityComponent;
 import org.spout.api.entity.Player;
 import org.spout.api.geo.discrete.Point;
 
+/**
+ * This file is an example, and will be removed once the Plugin is ready for
+ * release.
+ * @author tenowg
+ */
 public class PlayerRegionComponent extends EntityComponent {
 
 	private int tickcount = 0;
@@ -17,12 +23,14 @@ public class PlayerRegionComponent extends EntityComponent {
 			Player player = (Player) getOwner();
 			WorldRegionComponent worldComp = getOwner().getWorld().getComponentHolder().get(WorldRegionComponent.class);
 
-			Region region = worldComp.inRegion(player.getScene().getPosition());
+			Set<Region> region = worldComp.getRegion(player.getScene().getPosition());
 			
 			long end = System.nanoTime();
 			
 			if (region != null) {
-				//player.sendMessage("IN " + region.getName());
+				for (Region reg : region) {
+					player.sendMessage("IN " + reg.getName());
+				}
 			} else {
 				//lastsafe = player.getScene().getPosition();
 			}
--- src/main/java/com/thedemgel/regions/component/WorldRegionComponent.java
+++ src/main/java/com/thedemgel/regions/component/WorldRegionComponent.java
@@ -3,11 +3,15 @@ package com.thedemgel.regions.component;
 import com.thedemgel.regions.data.BBox;
 import com.thedemgel.regions.data.PointMap;
 import com.thedemgel.regions.data.Region;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 import me.dzineit.selectionapi.SelectionPlayer;
+import org.apache.commons.collections.set.UnmodifiableSet;
 import org.spout.api.Spout;
 import org.spout.api.component.type.WorldComponent;
 import org.spout.api.entity.Player;
@@ -15,37 +19,70 @@ import org.spout.api.event.Event;
 import org.spout.api.geo.discrete.Point;
 import org.spout.api.util.list.concurrent.ConcurrentList;
 
+/**
+ * WorldRegionComponent is where all the regions are stored, and 99% of all
+ * Regions are accessed and modified.
+ *
+ * Should be called with
+ * world.getComponentHolder().get(WorldRegionComponent.class) there should be no
+ * need to access a region directly, if something is missing and you find you
+ * need to access a region, feature, or feature parent directly, please notify
+ * me and I will fix it.
+ *
+ * @author tenowg
+ */
 public class WorldRegionComponent extends WorldComponent {
 
-	private ConcurrentMap<String, Region> regions = new ConcurrentSkipListMap<String, Region>(String.CASE_INSENSITIVE_ORDER);
+	private ConcurrentMap<UUID, Region> regions = new ConcurrentSkipListMap<UUID, Region>();
 	private ConcurrentMap<PointMap, ConcurrentList<Region>> xregions = new ConcurrentHashMap<PointMap, ConcurrentList<Region>>();
-	
+
+	/**
+	 * Will add an already created Region to the Maps and parse its
+	 * PointMaps and effectively add the region to the Map. (Should only be
+	 * called from createRegion, or if the Region Object has been FULLY
+	 * created first)
+	 *
+	 * @param region
+	 */
 	public void addRegion(Region region) {
 		if (!regions.containsValue(region)) {
-			getData().get("regions", new ConcurrentSkipListMap<String, Region>(String.CASE_INSENSITIVE_ORDER)).put(region.getName(), region);
-			regions = getData().get("regions", new ConcurrentSkipListMap<String, Region>(String.CASE_INSENSITIVE_ORDER));
+			getData().get("regions", new ConcurrentSkipListMap<UUID, Region>()).put(region.getUUID(), region);
+			regions = getData().get("regions", new ConcurrentSkipListMap<UUID, Region>());
 		}
-		
+
 		updateRegion(region);
 	}
 
+	/**
+	 * Will completely remove a region from all Maps effectively removing it
+	 * from the game.
+	 *
+	 * @param region
+	 */
 	public void removeRegion(Region region) {
-		for(PointMap mpoint : region.getPointCache()) {
+		for (PointMap mpoint : region.getPointCache()) {
 			ConcurrentList<Region> regs = xregions.get(mpoint);
 			regs.remove(region);
 		}
-		
-		regions.remove(region.getName());
+
+		regions.remove(region.getUUID());
 	}
-	
+
+	/**
+	 * Should be called after the size of a Region has been changed. This
+	 * will "fix" the PointMaps and regenerate the PointMap cache of the
+	 * region.
+	 *
+	 * @param region Region after it has been updated with new Coords.
+	 */
 	public void updateRegion(Region region) {
-		for(PointMap mpoint : region.getPointCache()) {
+		for (PointMap mpoint : region.getPointCache()) {
 			ConcurrentList<Region> regs = xregions.get(mpoint);
 			regs.remove(region);
 		}
-		
+
 		region.resetPointCache();
-		
+
 		int i, ii, iii;
 
 		for (i = (int) (region.getRegion().getLowX() / 100); i <= (int) (region.getRegion().getHighX() / 100); i++) {
@@ -63,16 +100,43 @@ public class WorldRegionComponent extends WorldComponent {
 			}
 		}
 	}
-	
+
 	/**
-	 * Gets a region by name.
+	 * Gets a region by name. Not the preferred method of retrieving a
+	 * region. As names can change, and more processor is needed.
+	 *
 	 * @param name
-	 * @return 
+	 * @return Region whose name is name
 	 */
 	public Region getRegion(String name) {
-		return getData().get("regions", new ConcurrentSkipListMap<String, Region>(String.CASE_INSENSITIVE_ORDER)).get(name);
+		for (Region region : regions.values()) {
+			if (region.getName().equalsIgnoreCase(name)) {
+				return region;
+			}
+		}
+
+		return null;
 	}
 
+	/**
+	 * Gets a region by UUID. Developers should store UUIDs instead of Names
+	 * in their plugins As they should never change once the regions are
+	 * created, and use less time to search.
+	 *
+	 * @param uuid UUID of the region being searched.
+	 * @return Region whose UUID is uuid
+	 */
+	public Region getRegion(UUID uuid) {
+		return regions.get(uuid);
+	}
+
+	/**
+	 * Creates a Region from SelectPlayer selections.
+	 *
+	 * @param player The player creating the Region.
+	 * @param name Name of the Region
+	 * @return The Created Region
+	 */
 	public Region createRegion(Player player, String name) {
 		SelectionPlayer selplayer = player.get(SelectionPlayer.class);
 		Spout.getLogger().info(selplayer.toString());
@@ -85,60 +149,70 @@ public class WorldRegionComponent extends WorldComponent {
 	}
 
 	/**
-	 * Will return the Region that contains a specific point.
+	 * Will return the Regions that contains a specific point.
+	 * The return is unmodifiable.
+	 *
 	 * @param point
-	 * @return 
+	 * @return Unmodifiable Set of Regions that contain the point
 	 */
-	public Region inRegion(Point point) {
+	public Set<Region> getRegion(Point point) {
 		PointMap mpoint = new PointMap(point);
+		Set<Region> regionsRet = new HashSet<Region>();
 		if (xregions.containsKey(mpoint)) {
 			for (Region region : xregions.get(mpoint)) {
 				if (region.getRegion().containsPoint(point)) {
-					return region;
+					regionsRet.add(region);
 				}
 			}
 		}
 
-		return null;
+		return Collections.unmodifiableSet(regionsRet);
 	}
 
 	public void init() {
-		regions = getData().get("regions", new ConcurrentSkipListMap<String, Region>(String.CASE_INSENSITIVE_ORDER));
+		regions = getData().get("regions", new ConcurrentSkipListMap<UUID, Region>());
 		for (Region region : regions.values()) {
 			addRegion(region);
 		}
 	}
-	
+
 	/**
-	 * Attempts to call Features from all RAZs worldwide
-	 * (care needs to be taken when using this)
-	 * @param event 
+	 * Attempts to call Features from all RAZs worldwide (care needs to be
+	 * taken when using this)
+	 *
+	 * @param event
 	 */
 	public void execute(Event event) {
 		for (Region reg : regions.values()) {
 			reg.getHolder().execute(event, reg);
 		}
 	}
-	
+
 	/**
-	 * Method to call Features from Point (preferred method)
+	 * Method to call Features from a Point in a Region.
+	 *
+	 * Use to call feature execute from other directly. It is possible to
+	 * send NULL events.
+	 *
 	 * @param event
 	 * @param point
 	 */
 	public void execute(Event event, Point point) {
-		Region region = inRegion(point);
-		if (region != null) {
-			region.getHolder().execute(event, region);
+		for (Region region : getRegion(point)) {
+			if (region != null) {
+				region.getHolder().execute(event, region);
+			}
 		}
 	}
-	
+
 	/**
 	 * Method to call Features from Region Name
-	 * 
-	 * Use to call feature execute from other directly.
-	 * It is possible to send NULL events.
-	 * @param event
-	 * @param regionName 
+	 *
+	 * Use to call feature execute from other directly. It is possible to
+	 * send NULL events. (Not recommended)
+	 *
+	 * @param event Event to be called
+	 * @param regionName Name of the region
 	 */
 	public void execute(Event event, String regionName) {
 		Region region = getRegion(regionName);
@@ -146,10 +220,42 @@ public class WorldRegionComponent extends WorldComponent {
 			region.getHolder().execute(event, region);
 		}
 	}
-	
+
+	/**
+	 * Method to call Features from a Region's UUID
+	 *
+	 * Use to call feature execute from other directly. It is possible to
+	 * send NULL events. (Preferred method)
+	 *
+	 * @param event Event to be called
+	 * @param uuid UUID of the Region
+	 */
+	public void execute(Event event, UUID uuid) {
+		Region region = getRegion(uuid);
+		if (region != null) {
+			region.getHolder().execute(event, region);
+		}
+	}
+
+	/**
+	 * Method to call Features from a Region with Region OBJECT.
+	 *
+	 * Use to call feature execute from other directly. It is possible to
+	 * send NULL events. (Fastest way if you have the region object)
+	 *
+	 * @param event Event to be called
+	 * @param region The Region OBJECT
+	 */
+	public void execute(Event event, Region region) {
+		if (region != null) {
+			region.getHolder().execute(event, region);
+		}
+	}
+
 	/**
 	 * Will attempt to fire all OnTicks in all Features worldwide.
-	 * @param dt 
+	 *
+	 * @param dt
 	 */
 	@Override
 	public void onTick(float dt) {
