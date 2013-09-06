45acd3a41ad6ad918cbdbeb1d62cbfdc6f94bdfc
Craig Russell <tenowg@gmail.com>
2013-09-06 03:35:23 -0400

    Working Signed-off-by:Craig Russell <tenowg@gmail.com>

--- src/main/java/com/thedemgel/regions/FeatureRegister.java
+++ src/main/java/com/thedemgel/regions/FeatureRegister.java
@@ -4,72 +4,70 @@
  */
 package com.thedemgel.regions;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.thedemgel.regions.annotations.FeatureName;
 import com.thedemgel.regions.data.PluginFeatures;
 import com.thedemgel.regions.feature.Feature;
 import com.thedemgel.regions.parser.EventParser;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+
 import org.spout.api.plugin.Plugin;
 
 /**
  * Handle registering features and retrieving features.
  */
 public class FeatureRegister {
-
-	//private final Regions plugin;
 	private final RegisterEvents eventRegister;
-	private EventParser eventParser;
-	
+	private final EventParser eventParser;
+
 	private Map<Plugin, PluginFeatures> features = new ConcurrentHashMap<>();
-	
-	public FeatureRegister(Regions plugin) {
-		//this.plugin = plugin;
+
+	public FeatureRegister(final Regions plugin) {
 		eventParser = new EventParser();
 		eventRegister = new RegisterEvents(plugin);
 	}
-	
-	public String parseName(Class<? extends Feature> feature) {
+
+	public final String parseName(Class<? extends Feature> feature) {
 		String name = "";
-		
+
 		if (feature.isAnnotationPresent(FeatureName.class)) {
 			FeatureName fname = feature.getAnnotation(FeatureName.class);
 			name = fname.value();
 		}
-		
+
 		if (name.equals("")) {
 			name = feature.getSimpleName();
 		}
-		
+
 		return name;
 	}
-	
-	public void registerFeature(Plugin plugin, Class<? extends Feature> feature) {
+
+	public final void registerFeature(Plugin plugin, Class<? extends Feature> feature) {
 		registerFeature(plugin, feature, eventParser);
 	}
-	
-	public void registerFeature(Plugin plugin, Class<? extends Feature> feature, EventParser parser) {
+
+	public final void registerFeature(Plugin plugin, Class<? extends Feature> feature, EventParser parser) {
 		if (!features.containsKey(plugin)) {
 			features.put(plugin, new PluginFeatures());
 		}
 		features.get(plugin).getFeatures().put(parseName(feature), feature);
-		
+
 		eventRegister.registerEvents(feature, parser);
 	}
-	
-	public Map<Plugin, PluginFeatures> getFeatures() {
+
+	public final Map<Plugin, PluginFeatures> getFeatures() {
 		return features;
 	}
-	
-	public Class<? extends Feature> getFeature(Plugin plugin, Class<? extends Feature> feature) {
+
+	public final Class<? extends Feature> getFeature(Plugin plugin, Class<? extends Feature> feature) {
 		return getFeature(plugin, parseName(feature));
 	}
-	
-	public Class<? extends Feature> getFeature(Plugin plugin, String SimpleName) {
+
+	public final Class<? extends Feature> getFeature(Plugin plugin, String simpleName) {
 		if (features.containsKey(plugin)) {
-			return features.get(plugin).getFeatures().get(SimpleName);
+			return features.get(plugin).getFeatures().get(simpleName);
 		}
-		
 		return null;
 	}
 }
